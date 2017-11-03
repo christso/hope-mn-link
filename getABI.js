cd154493ecc8151c70e9af28cdcabe3da0d9c0ba
Christopher Tso <christso@gmail.com>
2017-11-03 21:49:20 +1100

    update ABI

--- getABI.js
+++ getABI.js
@@ -1,465 +1,496 @@
 function abi() {
   return [
     {
-      constant: true,
-      inputs: [],
-      name: "name",
-      outputs: [
+      "constant": true,
+      "inputs": [],
+      "name": "name",
+      "outputs": [
         {
-          name: "",
-          type: "string"
+          "name": "",
+          "type": "string"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: false,
-      inputs: [
+      "constant": false,
+      "inputs": [
         {
-          name: "_spender",
-          type: "address"
+          "name": "_spender",
+          "type": "address"
         },
         {
-          name: "_value",
-          type: "uint256"
+          "name": "_value",
+          "type": "uint256"
         }
       ],
-      name: "approve",
-      outputs: [
+      "name": "approve",
+      "outputs": [
         {
-          name: "",
-          type: "bool"
+          "name": "",
+          "type": "bool"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: false,
-      inputs: [
+      "constant": false,
+      "inputs": [
         {
-          name: "_minter",
-          type: "address"
+          "name": "_minter",
+          "type": "address"
         }
       ],
-      name: "allowMinter",
-      outputs: [
+      "name": "allowMinter",
+      "outputs": [
         {
-          name: "",
-          type: "bool"
+          "name": "",
+          "type": "bool"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: true,
-      inputs: [],
-      name: "totalSupply",
-      outputs: [
+      "constant": true,
+      "inputs": [],
+      "name": "totalSupply",
+      "outputs": [
         {
-          name: "",
-          type: "uint256"
+          "name": "",
+          "type": "uint256"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: false,
-      inputs: [
+      "constant": false,
+      "inputs": [
         {
-          name: "_from",
-          type: "address"
+          "name": "_from",
+          "type": "address"
         },
         {
-          name: "_to",
-          type: "address"
+          "name": "_to",
+          "type": "address"
         },
         {
-          name: "_value",
-          type: "uint256"
+          "name": "_value",
+          "type": "uint256"
         }
       ],
-      name: "transferFrom",
-      outputs: [
+      "name": "transferFrom",
+      "outputs": [
         {
-          name: "",
-          type: "bool"
+          "name": "",
+          "type": "bool"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: true,
-      inputs: [],
-      name: "decimals",
-      outputs: [
+      "constant": true,
+      "inputs": [],
+      "name": "decimals",
+      "outputs": [
         {
-          name: "",
-          type: "uint256"
+          "name": "",
+          "type": "uint256"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: true,
-      inputs: [
+      "constant": true,
+      "inputs": [],
+      "name": "version",
+      "outputs": [
         {
-          name: "_owner",
-          type: "address"
+          "name": "",
+          "type": "string"
         }
       ],
-      name: "balanceOf",
-      outputs: [
-        {
-          name: "balance",
-          type: "uint256"
-        }
-      ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: false,
-      inputs: [
+      "constant": true,
+      "inputs": [
         {
-          name: "_recipients",
-          type: "address[]"
+          "name": "_owner",
+          "type": "address"
+        }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+        {
+          "name": "balance",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_recipients",
+          "type": "address[]"
         },
         {
-          name: "_values",
-          type: "uint256[]"
+          "name": "_values",
+          "type": "uint256[]"
         }
       ],
-      name: "reverseBatchTransfer",
-      outputs: [
+      "name": "reverseBatchTransfer",
+      "outputs": [
         {
-          name: "",
-          type: "bool"
+          "name": "",
+          "type": "bool"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: false,
-      inputs: [
+      "constant": false,
+      "inputs": [
         {
-          name: "_minter",
-          type: "address"
+          "name": "_minter",
+          "type": "address"
         }
       ],
-      name: "disallowMinter",
-      outputs: [
+      "name": "disallowMinter",
+      "outputs": [
         {
-          name: "",
-          type: "bool"
+          "name": "",
+          "type": "bool"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: false,
-      inputs: [
+      "constant": false,
+      "inputs": [
         {
-          name: "_recipients",
-          type: "address[]"
+          "name": "_recipients",
+          "type": "address[]"
         },
         {
-          name: "_values",
-          type: "uint256[]"
+          "name": "_values",
+          "type": "uint256[]"
         }
       ],
-      name: "batchTransfer",
-      outputs: [
+      "name": "batchTransfer",
+      "outputs": [
         {
-          name: "",
-          type: "bool"
+          "name": "",
+          "type": "bool"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: true,
-      inputs: [],
-      name: "owner",
-      outputs: [
+      "constant": true,
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
         {
-          name: "",
-          type: "address"
+          "name": "",
+          "type": "address"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: false,
-      inputs: [
+      "constant": false,
+      "inputs": [
         {
-          name: "_from",
-          type: "address"
+          "name": "_from",
+          "type": "address"
         },
         {
-          name: "_value",
-          type: "uint256"
+          "name": "_value",
+          "type": "uint256"
         },
         {
-          name: "_dmdAddress",
-          type: "bytes32"
+          "name": "_dmdAddress",
+          "type": "bytes32"
         }
       ],
-      name: "burnFrom",
-      outputs: [
+      "name": "burnFrom",
+      "outputs": [
         {
-          name: "",
-          type: "bool"
+          "name": "",
+          "type": "bool"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: true,
-      inputs: [],
-      name: "symbol",
-      outputs: [
+      "constant": true,
+      "inputs": [],
+      "name": "symbol",
+      "outputs": [
         {
-          name: "",
-          type: "string"
+          "name": "",
+          "type": "string"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: true,
-      inputs: [],
-      name: "totalInitialSupply",
-      outputs: [
+      "constant": true,
+      "inputs": [],
+      "name": "totalInitialSupply",
+      "outputs": [
         {
-          name: "",
-          type: "uint256"
+          "name": "",
+          "type": "uint256"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: false,
-      inputs: [
+      "constant": false,
+      "inputs": [
         {
-          name: "_reward",
-          type: "uint256"
+          "name": "_reward",
+          "type": "uint256"
         }
       ],
-      name: "mint",
-      outputs: [
+      "name": "mint",
+      "outputs": [
         {
-          name: "",
-          type: "bool"
+          "name": "",
+          "type": "bool"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: false,
-      inputs: [
+      "constant": false,
+      "inputs": [
         {
-          name: "_from",
-          type: "address"
+          "name": "_from",
+          "type": "address"
         },
         {
-          name: "_to",
-          type: "address"
+          "name": "_to",
+          "type": "address"
         },
         {
-          name: "_value",
-          type: "uint256"
+          "name": "_value",
+          "type": "uint256"
         }
       ],
-      name: "ownerTransfer",
-      outputs: [
+      "name": "ownerTransfer",
+      "outputs": [
         {
-          name: "",
-          type: "bool"
+          "name": "",
+          "type": "bool"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: false,
-      inputs: [
+      "constant": false,
+      "inputs": [
         {
-          name: "_to",
-          type: "address"
+          "name": "_to",
+          "type": "address"
         },
         {
-          name: "_value",
-          type: "uint256"
+          "name": "_value",
+          "type": "uint256"
         }
       ],
-      name: "transfer",
-      outputs: [
+      "name": "transfer",
+      "outputs": [
         {
-          name: "",
-          type: "bool"
+          "name": "",
+          "type": "bool"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: false,
-      inputs: [
+      "constant": false,
+      "inputs": [
         {
-          name: "_value",
-          type: "uint256"
+          "name": "_value",
+          "type": "uint256"
         },
         {
-          name: "_dmdAddress",
-          type: "bytes32"
+          "name": "_dmdAddress",
+          "type": "bytes32"
         }
       ],
-      name: "burn",
-      outputs: [
+      "name": "burn",
+      "outputs": [
         {
-          name: "",
-          type: "bool"
+          "name": "",
+          "type": "bool"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: true,
-      inputs: [
+      "constant": true,
+      "inputs": [
         {
-          name: "_owner",
-          type: "address"
-        },
-        {
-          name: "_spender",
-          type: "address"
+          "name": "_minter",
+          "type": "address"
         }
       ],
-      name: "allowance",
-      outputs: [
+      "name": "canMint",
+      "outputs": [
         {
-          name: "remaining",
-          type: "uint256"
+          "name": "",
+          "type": "bool"
         }
       ],
-      payable: false,
-      type: "function"
+      "payable": false,
+      "type": "function"
     },
     {
-      constant: false,
-      inputs: [
+      "constant": true,
+      "inputs": [
         {
-          name: "newOwner",
-          type: "address"
+          "name": "_owner",
+          "type": "address"
+        },
+        {
+          "name": "_spender",
+          "type": "address"
         }
       ],
-      name: "transferOwnership",
-      outputs: [],
-      payable: false,
-      type: "function"
-    },
-    {
-      inputs: [],
-      payable: false,
-      type: "constructor"
-    },
-    {
-      anonymous: false,
-      inputs: [
+      "name": "allowance",
+      "outputs": [
         {
-          indexed: true,
-          name: "burner",
-          type: "address"
-        },
-        {
-          indexed: false,
-          name: "dmdAddress",
-          type: "bytes32"
-        },
-        {
-          indexed: false,
-          name: "value",
-          type: "uint256"
+          "name": "remaining",
+          "type": "uint256"
         }
       ],
-      name: "Burn",
-      type: "event"
+      "payable": false,
+      "type": "function"
     },
     {
-      anonymous: false,
-      inputs: [
+      "constant": false,
+      "inputs": [
         {
-          indexed: true,
-          name: "_address",
-          type: "address"
-        },
-        {
-          indexed: false,
-          name: "_reward",
-          type: "uint256"
+          "name": "newOwner",
+          "type": "address"
         }
       ],
-      name: "Mint",
-      type: "event"
+      "name": "transferOwnership",
+      "outputs": [],
+      "payable": false,
+      "type": "function"
     },
     {
-      anonymous: false,
-      inputs: [
-        {
-          indexed: true,
-          name: "owner",
-          type: "address"
-        },
-        {
-          indexed: true,
-          name: "spender",
-          type: "address"
-        },
-        {
-          indexed: false,
-          name: "value",
-          type: "uint256"
-        }
-      ],
-      name: "Approval",
-      type: "event"
+      "inputs": [],
+      "payable": false,
+      "type": "constructor"
     },
     {
-      anonymous: false,
-      inputs: [
+      "anonymous": false,
+      "inputs": [
         {
-          indexed: true,
-          name: "from",
-          type: "address"
+          "indexed": true,
+          "name": "burner",
+          "type": "address"
         },
         {
-          indexed: true,
-          name: "to",
-          type: "address"
+          "indexed": false,
+          "name": "dmdAddress",
+          "type": "bytes32"
         },
         {
-          indexed: false,
-          name: "value",
-          type: "uint256"
+          "indexed": false,
+          "name": "value",
+          "type": "uint256"
         }
       ],
-      name: "Transfer",
-      type: "event"
+      "name": "Burn",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "_address",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "_reward",
+          "type": "uint256"
+        }
+      ],
+      "name": "Mint",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Approval",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Transfer",
+      "type": "event"
     }
   ];
 }

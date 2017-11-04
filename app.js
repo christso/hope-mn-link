8ce73980668bee6ec99e7d319bd23f3eb13707e7
Christopher Tso <christso@gmail.com>
2017-11-04 13:30:36 +1100

    add comment on cryptoid API

--- app.js
+++ app.js
@@ -24,8 +24,10 @@ if (hdmdVersionDeployed == hdmdVersion) {
 }
 
 // TODO: Create DMD listener
-// we will call the API every hour
-// http://chainz.cryptoid.info/dmd/api.dws?q=getbalance&a=dH4bKCoyNj9BzLuyU4JvhwvhYs7cnogDVb
+// Call the API every hour. 
+// Create MongoDB document for each new DMD transaction, which stores the txn hash.
+// List Balance: http://chainz.cryptoid.info/dmd/api.dws?q=getbalance&a=dH4bKCoyNj9BzLuyU4JvhwvhYs7cnogDVb
+// List Txns: https://chainz.cryptoid.info/explorer/address.summary.dws?coin=dmd&id=12829&r=25294&fmt.js
 
 // Create HDMD listener
 

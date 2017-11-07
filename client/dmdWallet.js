886f5a4950f2d47f305c284e7785141ce21796f2
Christopher Tso <christso@gmail.com>
2017-11-08 01:57:44 +1100

    add proof-of-concept for sending DMD

---
+++ client/dmdWallet.js
@@ -0,0 +1,30 @@
+var bitcoin = require('bitcoin');
+
+var client = new bitcoin.Client({
+    host: '127.0.0.1',
+    //port: 51473,
+    port: 17772,
+    user: 'testuser',
+    pass: 'hope2017'
+});
+
+client.getDifficulty(function (err, difficulty) {
+    if (err) {
+        return console.error(err);
+    }
+
+    console.log('Difficulty: ' + difficulty);
+});
+
+client.getBalance('*', 6, function (err, balance) {
+    if (err) return console.log(err);
+    console.log('Balance:', balance);
+});
+
+client.sendToAddress('dQmpKBcneq1ZF27iuJsUm8dQ2QkUriKWy3', 0.1, function (err, difficulty) {
+    if (err) {
+        return console.error(err);
+    }
+
+    console.log('Sent: ' + difficulty);
+});

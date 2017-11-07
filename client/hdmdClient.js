991d0e367970f2524078d274f66ecc2f04ae673d
BunnyCode <theodor.borgman@gmail.com>
2017-11-07 23:05:44 +0100

    Burn now working

--- client/hdmdClient.js
+++ client/hdmdClient.js
@@ -2,6 +2,7 @@ const Web3 = require('web3');
 const abi = require('./hdmdABI')();
 const config = require('../config');
 var hdmdTxns = require('../models/hdmdTxn');
+var hdmdBurn = require('../models/hdmdBurn');
 
 const hdmdVersion = config.hdmdVersion;
 const ethNodeAddress = config.ethNodeAddress;
@@ -35,35 +36,74 @@ console.log(`Listening for changes on the blockchain on node ${ethNodeAddress}`)
 var evntAll = hdmdContract.allEvents({}, { fromBlock: 0, toBlock: 'latest' });
 evntAll.watch(function (error, result) {
     console.log('listening for changes on the blockchain...');
-    console.log(arguments[1]); // for Debugging
-    saveTxns(parseTxInfo(arguments[1]));
+    console.log('Debugging', arguments[1]); // for Debugging
+    // saveTxns(parseTxInfo(arguments[1]));
+    saveTxns(arguments[1]);
 });
 
 
 
 function saveTxns(newTxns) {
-    console.log(newTxns);
     // Create new DMD txn and save to DB
-    var newTransaction = hdmdTxns({
-        txnHash: newTxns.txnHash,
-        blockNumber: newTxns.blockNumber,
-        amount: newTxns.amount,
-        dmd_txnHash: newTxns.dmd_txn
-    });
+    if (newTxns.event === 'Mint') {
+        // let parsedEvent = parseMint(newTxns);
+        var newTransaction = hdmdTxns({
+            // txnHash: parsedEvent.txnHash,
+            // blockNumber: parsedEvent.blockNumber,
+            // amount: parsedEvent.amount,
+            // dmd_txnHash: parsedEvent.dmd_txn
+            txnHash: newTxns.args.transactionHash,
+            blockNumber: newTxns.args.blockNumber,
+            amount: newTxns.args.args._reward.c[0],
+            dmd_txnHash: newTxns.args.args._dmdTx
+        });
+        saveToMongo(newTransaction);
+    }
+    if (newTxns.event === 'Burn') {
+        console.log('Burn baby Burn!');
+        console.log(newTxns);
+        // let parsedEvent = parseBurn(newTxns);
+        var newTransaction = hdmdBurn({
+            burner: newTxns.args.burner,
+            dmdAddress: newTxns.args.dmdAddress,
+            txnHash: newTxns.txnHash,
+            blockNumber: newTxns.blockNumber,
+            amount: newTxns.amount,
+            txnHash: newTxns.dmd_txn
+        });
+        saveToMongo(newTransaction);
+    }
 
-    newTransaction.save().then((doc) => {
+
+
+
+}
+function saveToMongo(event) {
+    event.save().then((doc) => {
         console.log('saved', doc)
     }, (err) => {
         console.log('Unable to save data')
     });
 }
 
-
 // Pass this info to saveTxns so it can be put in to Mongo
 // This is a bit messy to be honest but it gets the DMD Txn.
 // Play around with it for a bit. your map solution looks nice to. but i dont know
 // if that would work here, @Christso looks like you made a mapper further down.
-function parseTxInfo(args) {
+function parseMint(args) {
+    return {
+        txnHash: args.transactionHash,
+        blockNumber: args.blockNumber,
+        amount: args.args._reward.c[0],
+        dmd_txn: args.args._dmdTx
+
+        // Do we need these?
+        // address: arguments[1].address,
+        // eventType: arguments[1].event,
+        // hdmdTx: arguments[1].transactionHash,
+    };
+}
+function parseBurn(args) {
     return {
         txnHash: args.transactionHash,
         blockNumber: args.blockNumber,

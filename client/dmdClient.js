e8f60a3f8740e53edc1bdea77160b67f42016538
Christopher Tso <christso@gmail.com>
2017-11-14 23:19:25 +1100

    refactor dmdClient promises

--- client/dmdClient.js
+++ client/dmdClient.js
@@ -28,10 +28,7 @@ function parseRawTxns(rawTxns) {
 }
 
 function saveTxns(newTxns) {
-   return new Promise((resolve, reject) => {
-      // Create new DMD txn and save to DB
-      return dmdTxns.create(newTxns);
-   });
+   return dmdTxns.create(newTxns);
 }
 
 function getLastSavedTxn() {
@@ -42,6 +39,25 @@ function getLastSavedTxn() {
       .sort({ blockNumber: -1 })
       .limit(1)
       .exec();
+}
+
+function getLastSavedBlockNumber() {
+   return getLastSavedTxn()
+      .then(docs => {
+         // format value
+         let result = formatSavedBlockNumber(docs);
+         return Promise.resolve(result);
+      })
+      .catch(err => Promise.reject(err));
+}
+
+function formatSavedBlockNumber(docs) {
+   // format value
+   let lastBlockNumber = 0;
+   if (docs.length > 0) {
+      lastBlockNumber = docs[0].blockNumber;
+   }
+   return lastBlockNumber;
 }
 
 function saveTxns(lastBlockNumber) {
@@ -55,63 +71,30 @@ function saveTxns(lastBlockNumber) {
       // Create new DMD txn in MongoDB
       dmdTxns
          .create(newTxns)
-         .then(newlyCreated => resolve(newlyCreated))
-         .catch(err => reject(err));
+         .then(newlyCreated => Promise.resolve(newlyCreated));
+
+      // Invoke mint() where amount > 0
+
+      //   newTxns.forEach(function(dmdTxn) {
+      //      let amount = hdmdClient.getRawValue(dmdTxn.amount);
+      //      if (amount > 0) {
+      //         hdmdClient
+      //            .mint(amount, dmdTxn.txnHash)
+      //            .then(txnHash =>
+      //               Promise.resolve({ context: 'Mint', txnHash: txnHash })
+      //            )
+      //            .catch(err => Promise.reject({ context: 'Mint', error: err }));
+      //      } else if (amount < 0) {
+      //         // TODO: unmint
+      //      }
+      //   }, this);
    });
 }
 
 function downloadTxns() {
-   return new Promise((resolve, reject) => {
-      // Get last block stored in MongoDB
-      // then create txns that are after that block
-      getLastSavedTxn()
-         .then(docs => {
-            // format value
-            let lastBlockNumber = 0;
-            if (docs.length > 0) {
-               lastBlockNumber = docs[0].blockNumber;
-            }
-            createTxns(lastBlockNumber);
-         })
-         .catch(err => {
-            reject(err);
-         });
-
-      // Create txns with block after lastBlockNumber
-      let createTxns = lastBlockNumber =>
-         axios
-            .get(dmdUrl)
-            .then(function(response) {
-               // Parse CryptoID txns
-               let parsedTxns = parseRawTxns(response.data.tx);
-               let newTxns = parsedTxns.filter(txn => {
-                  return txn.blockNumber > lastBlockNumber;
-               });
-
-               // Create new DMD txn in MongoDB
-               dmdTxns
-                  .create(newTxns)
-                  .then(newlyCreated => resolve(newlyCreated))
-                  .catch(err => reject(err));
-
-               // Invoke mint() where amount > 0
-               /*
-                 newTxns.forEach(function (dmdTxn) {
-                     let amount = hdmdClient.getRawValue(dmdTxn.amount);
-                     if (amount > 0) {
-                         hdmdClient.mint(amount, dmdTxn.txnHash).then(txnHash =>
-                             Promise.resolve({context: 'Mint', txnHash: txnHash})
-                         ).catch(err => Promise.reject({context: 'Mint', error: err}));
-                     } else if (amount < 0) {
-                         // TODO: unmint
-                     }
-                 }, this);
-                 */
-            })
-            .catch(function(error) {
-               reject(error);
-            });
-   });
+   return getLastSavedBlockNumber().then(lastBlockNumber =>
+      saveTxns(lastBlockNumber)
+   );
 }
 
 var client = {
